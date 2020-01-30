b0bf2346faf0f28532141fb6fcf22a1456592ec3
Mahmood Janami <janami_m@Golestan.hosp>
2020-01-30 08:23:51 +0330

    Add project files.

---
+++ TestSysInfo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSysInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("SELECT * FROM " + comboBox1.Text);
+
+            foreach (var mo in searcher.Get())
+                foreach (PropertyData pd in mo.Properties)
+                    textBox1.Text += pd.Name + ":   " + pd.Value + Environment.NewLine ;
+            searcher.Get().
+        }
+    }
+}
